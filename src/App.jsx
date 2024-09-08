564e914809fbc6f513a985c4f969b72134ef2ba8
rafadinero22 <rafaeljesus694@outlook.com>
2024-09-08 06:45:08 -0500

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import Figure from './Figure'
+import Manga from './Manga'
+import Keychain from './Keychain'
+import Box from './Box'
+import Footer from './Footer'
+import Header from './Header'
+import SmallFigure from './SmallFigure'
+
+function App() {
+  return (
+    <main>
+      <Header />
+      <div>
+        <Figure />
+      </div>
+      <div>
+        <SmallFigure/>
+      </div>
+      <div>
+        <Box />
+      </div>
+      <div>
+        <Manga />
+      </div>
+      <div>
+        <Keychain />
+      </div>
+      <Footer />
+    </main>
+  )
+}
+
+export default App
